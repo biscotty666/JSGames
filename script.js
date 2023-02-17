5388c8e24690afcacb300ad1a057a03fffd1b9f5
biscotty666 <biscotty666@gmail.com>
2023-02-16 23:10:22 -0700

    Lesson 3 and 4

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
   canvas.width = 1280;
   canvas.height = 720;
 
+  ctx.fillStyle = 'white';
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = 'white'
+
   class Player {
-    constructor(game){
+    constructor(game) {
       this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collsionRadius = 50;
+    }
+    draw(context) {
+      context.beginPath();
+      context.arc(this.collisionX, this.collisionY, this.collsionRadius, 0, Math.PI * 2);
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore();
+      // wrapping with save/restore scopes variable changes
+      context.stroke();
     }
   }
 
   class Game {
-    constructor(canvas){
+    constructor(canvas) {
       this.canvas = canvas;
       this.width = this.canvas.width;
       this.height = this.canvas.height;
       this.player = new Player(this);
+      this.mouse = {
+        x: this.width * 0.5,
+        y: this.height * 0.5,
+        pressed: false
+      }
+
+      canvas.addEventListener('mousedown', e => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = true;
+      })
+      canvas.addEventListener('mouseup', e => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = false;
+      })
+      canvas.addEventListener('mousemove', e => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        console.log(this.mouse.x)
+      })
+    }
+    render(context) {
+      this.player.draw(context);
     }
   }
 
   const game = new Game(canvas);
+  game.render(ctx);
   console.log(game);
 })
